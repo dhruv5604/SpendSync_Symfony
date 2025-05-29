e2c2710ffb5bf5df207e5802a27d10ea1b4416b8
dhruv5604 <dhruvsolanki5604@gmail.com>
2025-05-29 18:43:44 +0530

    added DashboardController.php

---
+++ src/Controller/DashboardController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractController
+{
+    #[Route('/dashboard', name: 'app_dashboard')]
+    public function dashboard()
+    {
+        return $this->render('dashboard/dashboard.html.twig');
+    }
+}
